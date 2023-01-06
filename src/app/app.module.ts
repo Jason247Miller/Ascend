a5c79245ff9f3ddb3a6e710dfebe7877be89c949
Jason Miller <jmiller@ceiamerica.com>
2023-01-06 10:49:31 -0500

    Added Components: home, knowledge-base, login, sign-up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { KnowledgeBaseComponent } from './knowledge-base/knowledge-base.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    SignUpComponent,
+    KnowledgeBaseComponent
   ],
   imports: [
     BrowserModule,
