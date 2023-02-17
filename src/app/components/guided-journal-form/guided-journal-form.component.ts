6d9bd2742c353446c87787041556507f7593e24e
Jason247Miller <jason.miller66@yahoo.com>
2023-02-17 09:20:35 -0500

    commit stashed changes

--- src/app/components/guided-journal-form/guided-journal-form.component.ts
+++ src/app/components/guided-journal-form/guided-journal-form.component.ts
@@ -1,13 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { IGuidedJournalEntry } from 'src/app/models/IGuidedJournalEntry';
+
 
 @Component({
   selector: 'app-guided-journal-form',
   templateUrl: './guided-journal-form.component.html',
   styleUrls: ['./guided-journal-form.component.less']
 })
-export class GuidedJournalFormComponent {
+export class GuidedJournalFormComponent implements OnInit {
+
+
+ngOnInit(): void {
+
+   
+ 
+}
 @Input() guidedJournalForm:FormGroup; 
 @Input() previousDailyReview:boolean; 
 @Input() journalEntries:IGuidedJournalEntry[]; 
